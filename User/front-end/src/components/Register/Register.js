7a97f9521892eec012c5deee79065b8b1a732927
BaoSkyFall <baoit128@gmail.com>
2019-12-04 23:25:06 +0700

    Update with variable in Back-end UserModel

--- User/front-end/src/components/Register/Register.js
+++ User/front-end/src/components/Register/Register.js
@@ -77,8 +77,9 @@ export default class Register extends Component {
 
       if (userId && password) {
         this.setState({ message: "Registering account" });
-        userService.register({ UserName: userId, Password: password, ConfirmPassword: password, Email: email, FirstName: firstName, LastName: lastName,Role:isTutor  }).then(
+        userService.register({ UserName: userId, Password: password, ConfirmPassword: password, Email: email, FirstName: firstName, LastName: lastName, Role: isTutor }).then(
           data => {
+            console.log('data:', data)
             if (data.user === null) {
               this.setState({ message: "This name is existed" });
             } else {
