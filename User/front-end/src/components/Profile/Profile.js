8b93182aeddf3f7316c54175c846e65f3224fc38
1612041_phbinh <1612041_phbinh>
2019-12-16 19:52:07 +0700

    add profile card

--- User/front-end/src/components/Profile/Profile.js
+++ User/front-end/src/components/Profile/Profile.js
@@ -1,6 +1,15 @@
 import React, { Component } from "react";
 import { userService } from "../../services/user.service";
-import { MDBContainer } from "mdbreact";
+import {
+  MDBContainer,
+  MDBRow,
+  MDBCol,
+  MDBCard,
+  MDBCardBody,
+  MDBInput,
+  MDBBtn,
+  MDBModalFooter
+} from "mdbreact";
 import { history } from "../../helpers/history";
 import "./Profile.css";
 import JWT from "jwt-decode";
@@ -13,6 +22,17 @@ export default class Profile extends Component {
       userInTokem: {
         nameId: "",
         role: ""
+      },
+      user: {
+        firstName: "phan",
+        lastName: "binh",
+        avatarUrl:
+          "https://www.viadelvino.com/wp-content/uploads/2016/02/photo.jpg.png",
+        email: "123456@gmail.com",
+        address: "124 duong so 1A",
+        description:
+          "Toi la 1 giao vien gioi, Toi la 1 giao vien gioi, Toi la 1 giao vien gioi, Toi la 1 giao vien gioi",
+        skillList: ["skill thu 1", "skill thu 2", "skill thu 3", "skill thu 4"]
       }
     };
   }
@@ -26,22 +46,26 @@ export default class Profile extends Component {
     });
   };
 
-  logout = () => {
-    userService.logout();
-    history.push("/login");
+  renderSkillListContainer = () => {
+    let skillListContainer = [];
+    const { user } = this.state;
+    user.skillList.forEach(element => {
+      skillListContainer.push(<div className="skill-tag"> {element} </div>);
+    });
+    return <div className="skill-list-container">{skillListContainer}</div>;
   };
   render() {
     console.log(this.state);
-    const { userInTokem } = this.state;
+    const { userInTokem, user } = this.state;
     let header = "";
-    if (userInTokem.role === "Tutor")
+    if (userInTokem.role === "Tutor") {
       header = (
         <div>
           <h2>This is Tutor profile page</h2>
           <h5>Hello {userInTokem.nameId}</h5>
         </div>
       );
-    else
+    } else
       header = (
         <div>
           <h2>This is student profile page</h2>
@@ -59,23 +83,38 @@ export default class Profile extends Component {
         <MyNavBar active="Profile" header={header}></MyNavBar>
 
         <main>
-          <MDBContainer className="text-center my-5">
-            <p align="justify">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
+          <MDBContainer className="text-center profile" id="container">
+            <MDBRow>
+              <MDBCol md="5" id="inner">
+                <MDBCard className="myCard">
+                  <MDBCardBody className="mx-4">
+                    <div className="text-center">
+                      <h3 className="dark-grey-text mb-5">
+                        <strong>Profile</strong>
+                      </h3>
+                    </div>
+                    <div className="image-wrapper">
+                      <img className="profile-image" src={user.avatarUrl}></img>
+                    </div>
+                    <div className="profile-details">
+                      <hr></hr>
+                      <span className="name">
+                        {user.lastName}, {user.firstName}
+                      </span>
+                      <hr></hr>
+                      <span>{user.description}</span>
+                      <hr></hr>
+                      <i className="fas fa-envelope"></i>
+                      <span>{user.email}</span>
+                      <hr></hr>
+                      <i className="fas fa-map-marker-alt"></i>
+                      <span>{user.address}</span>
+                      <div>{this.renderSkillListContainer()}</div>
+                    </div>
+                  </MDBCardBody>
+                </MDBCard>
+              </MDBCol>
+            </MDBRow>
           </MDBContainer>
         </main>
       </div>
